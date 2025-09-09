1e91933657837b3bf65fda895e8bcbd27074b6f0
DirtyData <51036884+AcidicSoil@users.noreply.github.com>
2025-09-09 15:01:12 -0500

    chore: add tsconfig for element

--- dist/element.d.ts
+++ dist/element.d.ts
@@ -1,14 +1,14 @@
 export declare class CodexSessionViewer extends HTMLElement {
-  private _session;
-  static get observedAttributes(): string[];
-  get src(): string | null;
-  set src(value: string | null);
-  get theme(): string | null;
-  set theme(value: string | null);
-  get session(): unknown;
-  set session(value: unknown);
-  connectedCallback(): void;
-  load(fileOrUrl: File | string): Promise<void>;
-  export(options?: unknown): Promise<void>;
+    private _session;
+    static get observedAttributes(): string[];
+    get src(): string | null;
+    set src(value: string | null);
+    get theme(): string | null;
+    set theme(value: string | null);
+    get session(): unknown;
+    set session(value: unknown);
+    connectedCallback(): void;
+    load(_fileOrUrl: File | string): Promise<void>;
+    export(_options?: unknown): Promise<void>;
 }
 export default CodexSessionViewer;
